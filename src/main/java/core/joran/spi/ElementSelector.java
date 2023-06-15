7efe1acb24dabdbd5fbb6c542f833474fee68077
Keunoh <rmsdhcjswo1@gmail.com>
2023-06-15 23:51:52 +0900

    logback core

--- src/main/java/core/joran/spi/ElementSelector.java
+++ src/main/java/core/joran/spi/ElementSelector.java
@@ -42,26 +42,88 @@ public class ElementSelector extends ElementPath {
 
         int minLen = (lSize <= rSize) ? lSize : rSize;
         int match = 0;
+
+        for (int i = 1; i <= minLen; i++) {
+            String l = this.partList.get(lSize - i);
+            String r = p.partList.get(rSize - i);
+
+            if (equalityCheck(i, r)) {
+                match++;
+            } else {
+                break;
+            }
+        }
+        return match;
+    }
+
+    public boolean isContainedIn(ElementPath p) {
+        if (p == null)
+            return false;
+        return p.toStableString().contains(toStableString());
+    }
+
+    public int getPrefixMatchLength(ElementPath p) {
+        if (p == null)
+            return 0;
+
+        int lSize = this.partList.size();
+        int rSize = partList.size();
+
+        if ((lSize == 0) || (rSize == 0))
+            return 0;
+
+        int minLen = (lSize <= rSize) ? lSize : rSize;
+        int match = 0;
+
+        for (int i = 0; i < minLen; i++) {
+            String l = this.partList.get(i);
+            String r = p.partList.get(i);
+
+            if (equalityCheck(l, r)) {
+                match++;
+            } else {
+                break;
+            }
+        }
+
+        return match;
+    }
+
+    private boolean equalityCheck(String x, String y) {
+        return x.equalsIgnoreCase(y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ((o == null) || !(o instanceof ElementSelector)) {
+            return false;
+        }
+
+        ElementSelector r = (ElementSelector) o;
+
+        if (r.size() != size()) {
+            return false;
+        }
+
+        int len = size();
+
+        for (int i = 0; i < len; i++) {
+            if (!equalityCheck(get(i), r.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hc = 0;
+        int len = size();
+
+        for (int i = 0; i < len; i++) {
+            hc ^= get(i).toLowerCase().hashCode();
+        }
+        return hc;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
